8b642b6391e8597a36407c4487efb413139065bf
Avnerus <avnerus@gmail.com>
2016-06-21 20:08:07 +0300

    oops I did it again

--- src/character.js
+++ src/character.js
@@ -1,29 +1,9 @@
-const MODEL_PATH = "assets/square/scene.json"
-
-export default class Square {
-    constructor() {
-        console.log("Square constructed!")
+export default class Character {
+    constructor(props) {
+        console.log("Character constructed!")
     }
-    init(scene,collisionManager,loadingManager) {
-        let loader = new THREE.ObjectLoader(loadingManager);
-        loader.load(MODEL_PATH,( obj ) => {
-            console.log("Loaded square ", obj);
-            obj.position.y = -1950;
-            obj.position.z = 1200;
-
-            scene.add(obj);
-            obj.updateMatrixWorld();
-            collisionManager.addBoundingBoxes(obj,scene);
-
-            this.squareMiddle  = obj.getObjectByName("MB_PS");
-            this.squareCenter  = new THREE.Vector3();
-            this.squareCenter.setFromMatrixPosition(this.squareMiddle.matrixWorld);
-        });
+    init(scene) {
     }
     update(dt) {
-    }
-
-    getCenterPosition() {
-        return this.squareCenter;
     }
 }
--- src/square.js
+++ src/square.js
@@ -1,9 +1,29 @@
-export default class Character {
-    constructor(props) {
-        console.log("Character constructed!")
+const MODEL_PATH = "assets/square/scene.json"
+
+export default class Square {
+    constructor() {
+        console.log("Square constructed!")
     }
-    init(scene) {
+    init(scene,collisionManager,loadingManager) {
+        let loader = new THREE.ObjectLoader(loadingManager);
+        loader.load(MODEL_PATH,( obj ) => {
+            console.log("Loaded square ", obj);
+            obj.position.y = -1950;
+            obj.position.z = 1200;
+
+            scene.add(obj);
+            obj.updateMatrixWorld();
+            collisionManager.addBoundingBoxes(obj,scene);
+
+            this.squareMiddle  = obj.getObjectByName("MB_PS");
+            this.squareCenter  = new THREE.Vector3();
+            this.squareCenter.setFromMatrixPosition(this.squareMiddle.matrixWorld);
+        });
     }
     update(dt) {
+    }
+
+    getCenterPosition() {
+        return this.squareCenter;
     }
 }
