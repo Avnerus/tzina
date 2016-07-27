55ac4119bc8361a689ee9aca104ea7d001e8f59a
Or Fleisher <contact@orfleisher.com>
2016-07-27 15:40:45 +0300

    3 Sound sources & cubes

--- src/sound_manager.js
+++ src/sound_manager.js
@@ -10,53 +10,84 @@ export default class SoundManager {
 
     }
 
-    init(camera) {
-      //LISTENER
-      this.listener = new THREE.AudioListener();
+    init() {
+        //LISTENER
+        this.listener = new THREE.AudioListener();
 
-      this.camera.add(this.listener);
+        this.camera.add(this.listener);
 
-      //SOUNDS
-      fountain = new THREE.PositionalAudio(this.listener);
-      fountain.position.set(3,15,95);
-      fountain.autoplay = false;
-      fountain.loop = true;
-/*
-      highway_1 = new THREE.PositionalAudio(this.listener);
-      highway_1.position.set(15,15,0);
-      highway_1.autoplay = false;
-      highway_1.loop = true;
+        //SOUNDS
+        fountain = new THREE.PositionalAudio(this.listener);
+        fountain.position.set(3, 15, 95);
+        fountain.autoplay = false;
+        fountain.loop = true;
 
-      highway_2 = new THREE.PositionalAudio(this.listener);
-      highway_2.position.set(3,15,95);
-      highway_2.autoplay = false;
-      highway_2.loop = true;
+        highway_1 = new THREE.PositionalAudio(this.listener);
+        highway_1.position.set(-25, 15, 0);
+        highway_1.autoplay = false;
+        highway_1.loop = true;
 
-      this.testCube = new THREE.Mesh( new THREE.BoxGeometry( 15, 15, 15 ), new THREE.MeshNormalMaterial() );
-      this.testCube.position.set(15,15,0);
-      this.scene.add(this.testCube);
-*/
-      this.scene.add(fountain);
+        highway_2 = new THREE.PositionalAudio(this.listener);
+        highway_2.position.set(3, 15, 170);
+        highway_2.autoplay = false;
+        highway_2.loop = true;
+
+        //DEBUG CUBES
+        this.testCubeone = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshNormalMaterial());
+        this.testCubeone.position.set(-25, 15, 0);
+        this.scene.add(this.testCubeone);
+
+        this.testCubetwo = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshNormalMaterial());
+        this.testCubetwo.position.set(30, 10, 190);
+        this.scene.add(this.testCubetwo);
+
+        //SOUND ADDING
+        this.scene.add(fountain);
+        this.scene.add(highway_1);
+        this.scene.add(highway_2);
+
+        //BUFFER THE SOUNDS INTO THE PROPER ELEMENTS
+        let loader = new THREE.AudioLoader();
+
+        // FOUNTAIN
+        loader.load(SOUND_PATH + 'ambient.ogg', function(audioBuffer) {
+            fountain.setBuffer(audioBuffer);
+        }, function() {
+            console.log('Fountain sound loaded');
+        });
+
+        // HIGHWAY ONE
+        loader.load(SOUND_PATH + 'ambient.ogg', function(audioBuffer) {
+            highway_1.setBuffer(audioBuffer);
+        }, function() {
+            console.log('Highway one sound loaded');
+        });
+
+        // HIGHWAY TWO
+        loader.load(SOUND_PATH + 'ambient.ogg', function(audioBuffer) {
+            highway_2.setBuffer(audioBuffer);
+        }, function() {
+            console.log('Highway two sound loaded');
+        });
 
 
-      // instantiate a loader
-      let loader = new THREE.AudioLoader();
-
-      // load the fountain
-      loader.load(SOUND_PATH + 'ambient.ogg', function ( audioBuffer ) {
-            fountain.setBuffer( audioBuffer );
-          }, function(){
-            console.log('Fountain sound loaded')
-          }
-        );
-      }
-
-    play(){
-    fountain.play();
 
     }
 
-    pause(){
-    fountain.pause();
+    play() {
+        fountain.play();
+        highway_1.play();
+        highway_2.play();
+
+    }
+
+    pause() {
+        fountain.pause();
+        fountain.currentTime = 0;
+        highway_1.pause();
+        highway_1.currentTime = 0;
+        highway_2.pause();
+        highway_2.currentTime = 0;
+
     }
 }
