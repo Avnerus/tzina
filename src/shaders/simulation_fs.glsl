87b15e0e1ae2657dbeccaab3ed39fce0a84906ae
jhclaura <jhclaura@gmail.com>
2016-07-31 13:21:48 -0400

    disable shader crazy velocity

--- src/shaders/simulation_fs.glsl
+++ src/shaders/simulation_fs.glsl
@@ -10,11 +10,12 @@ varying vec2 vUv;
 void main() {
     vec3 pos = texture2D( positions, vUv ).rgb;
     mat4 rotateY = rotationMatrix(vec3(0.0, 1.0, 0.0), random(vUv) * 0.01);
+    /*
     vec3 velocity = curlNoise(pos * 0.02) * 0.5;
     pos = pos + velocity; 
     if (pos.y > 10.0) {
         pos = velocity;
-    }
+    }*/
     /*
     pos.z -= (random(vUv) * 10.0);
     if (pos.z < -1000.0) {
