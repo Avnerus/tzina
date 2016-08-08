3560c29224f36c7621515c19b68b8aba58010e32
jhclaura <jhclaura@gmail.com>
2016-08-08 11:16:25 -0400

    test

--- src/shaders/simulation_fs.glsl
+++ src/shaders/simulation_fs.glsl
@@ -11,14 +11,14 @@ varying vec2 vUv;
 void main() {
     vec3 pos = texture2D( positions, vUv ).rgb;
     vec3 morphPos = texture2D( morphPositions, vUv ).rgb;
-    vec3 newPos = pos + (morphPos-pos) * timer;
+    pos = pos + (morphPos-pos) * timer;
 
     // mat4 rotateY = rotationMatrix(vec3(0.0, 1.0, 0.0), random(vUv) * 0.01);
     
-    vec3 velocity = curlNoise(newPos * 0.1) * 0.1;
-    newPos = newPos + velocity; 
-    if (newPos.y > 20.0) {
-        newPos = velocity;
+    vec3 velocity = curlNoise(pos * 0.1) * 0.2;
+    pos = pos + velocity; 
+    if (pos.y > 20.0) {
+        pos = velocity;
     }
     /*
     pos.z -= (random(vUv) * 10.0);
@@ -27,5 +27,5 @@ void main() {
     }*/
     //gl_FragColor = vec4( pos, 1.0 ) * rotateY;
 
-    gl_FragColor = vec4( newPos, 1.0 );
+    gl_FragColor = vec4( pos, 1.0 );
 }
