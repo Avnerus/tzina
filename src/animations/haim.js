3f3a280249468a275793602d6b627ce287720fe2
jhclaura <jhclaura@gmail.com>
2016-08-29 22:59:52 -0400

    enable haim particles, scale down haim particle texture

--- src/animations/haim.js
+++ src/animations/haim.js
@@ -10,42 +10,6 @@ export default class HaimAnimation extends THREE.Object3D {
     constructor( renderer ) {
         super();
         this.BASE_PATH = 'assets/animations/haim';
-
-        // FBO_PARTICLES
-        // ref: https://github.com/Avnerus/nao-game-client/blob/master/beam.js
-        const glslify = require('glslify');
-
-        // Shaders
-        this.render_fs = glslify('../shaders/haim/render_fs.glsl');
-        this.render_vs = glslify('../shaders/haim/render_vs.glsl');
-        this.simulation_fs = glslify('../shaders/haim/simulation_fs.glsl');
-        this.simulation_vs = glslify('../shaders/haim/simulation_vs.glsl');
-
-        this.width = 128;
-        this.height = 128;
-
-        this.renderer = renderer;
-        this.maxDepth = 50.0;
-
-        console.log("FBO Constructed!")
-    }
-
-    initParticles( geo ) {
-        let fboGeo = geo.clone();
-        fboGeo.applyMatrix( new THREE.Matrix4().makeScale(2,2,2) );
-        fboGeo.applyMatrix( new THREE.Matrix4().makeTranslation(-32, 20, 154) );
-        // fboGeo.applyMatrix( new THREE.Matrix4().makeRotationY(170 * Math.PI / 180) );
-
-        let data = new Float32Array( this.width * this.height * 3  );
-        let points = THREE.GeometryUtils.randomPointsInGeometry( fboGeo, this.width * this.height);
-        for ( var i = 0, j = 0, l = data.length; i < l; i += 3, j += 1 ) {
-            data[ i ] = points[ j ].x;
-            data[ i + 1 ] = points[ j ].y;
-            data[ i + 2 ] = points[ j ].z;
-        }
-        let positions = new THREE.DataTexture( data, this.width, this.height, THREE.RGBFormat, THREE.FloatType );
-        positions.needsUpdate = true;
-        return positions;
     }
 
     init(loadingManager) {
@@ -63,8 +27,8 @@ export default class HaimAnimation extends THREE.Object3D {
         // setup animation sequence
         // time: when to start animation, duration: how fast the animation is
         this.sequenceConfig = [
-            { time: 5,  anim: ()=>{this.tubeDown(1)} },
-            { time: 20, anim: ()=>{this.tubeOut(0.5)} }
+            { time: 10,  anim: ()=>{this.tubeDown(1)} },
+            { time: 30, anim: ()=>{this.tubeOut(0.5)} }
         ];
         this.nextAnim = null;
         this.completeSequenceSetup();
@@ -115,30 +79,11 @@ export default class HaimAnimation extends THREE.Object3D {
             }
             this.tubesVec.push( curve_vec );
         }
-        // let curve_vec = {}, curve_points=[];
-        // let c_size = Object.keys(curve_1).length;
-
-        // for(let i=0; i<c_size; i++) {
-        //     curve_vec[i] = [];
-        //     for(let j=0; j<curve_1[i].length; j++){
-        //         let newVector = new THREE.Vector3( Number(curve_1[i][j][0]),
-        //                                            Number(curve_1[i][j][1]),
-        //                                            Number(curve_1[i][j][2]) );
-        //         // scale down
-        //             newVector.multiplyScalar(0.5);
-        //         curve_vec[i].push( newVector );
-        //     }
-        // }
 
         // CLOUND
         let cloudTex = tex_loader.load( this.BASE_PATH + '/images/clouds.png' );
         // cloudTex.repeat.y = 2;
         this.cloudMat = new THREE.MeshBasicMaterial( {color: 0x00ffff, map: cloudTex, blending: "AdditiveBlending", transparent: true, side: THREE.DoubleSide, opacity: 0.4} );
-        // let cloudGeo = new THREE.PlaneGeometry(4,7);
-        // this.transY(cloudGeo, 3);
-        // this.cloud = new THREE.Mesh( cloudGeo, this.cloudMat );
-        // this.cloud.rotation.x = Math.PI/2;
-        // this.cloud.scale.multiplyScalar(0.01);
 
         // TUBE_BAG
         this.bag;
@@ -153,7 +98,6 @@ export default class HaimAnimation extends THREE.Object3D {
             this.bagTexs.push( bagTex );
             this.bagAniTexs.push( bagAniTex );
         }
-        // let bagTex = tex_loader.load(this.BASE_PATH + '/images/tubeBag.jpg');   
 
         // OUT_BOUND CURVES
         let curveOutData = [ {"0": [["-0.147", "1.266", "0.000"], ["0.065", "1.479", "0.000"], ["0.207", "1.585", "0.000"], ["0.384", "1.869", "0.000"], ["0.597", "2.188", "0.000"]], "1": [["-0.147", "1.266", "0.000"], ["0.233", "1.658", "0.000"], ["0.630", "2.155", "0.000"], ["1.191", "2.917", "0.000"], ["1.528", "3.654", "0.000"]], "2": [["-0.147", "1.266", "0.000"], ["0.758", "2.335", "0.000"], ["1.724", "3.843", "0.000"], ["2.172", "6.088", "0.000"], ["2.369", "8.359", "0.000"]], "3": [["-0.147", "1.266", "0.000"], ["1.466", "2.775", "0.000"], ["2.347", "5.554", "0.000"], ["2.663", "9.469", "0.000"], ["2.790", "12.589", "0.000"]], "4": [["-0.147", "1.266", "0.000"], ["1.896", "2.992", "0.000"], ["2.635", "6.535", "0.000"], ["2.875", "11.431", "0.000"], ["2.972", "14.938", "0.000"]]},
@@ -189,8 +133,6 @@ export default class HaimAnimation extends THREE.Object3D {
             this.bag = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( {map: this.bagTexs[0], transparent: true} ) );
             
             loader.load(this.BASE_PATH + "/models/cloud.json", (geometry, material) => {
-                // let cloudGeo = new THREE.PlaneGeometry(4,7);
-                // this.transY(cloudGeo, 3);
                 this.cloud = new THREE.Mesh( geometry, this.cloudMat );
                 // this.cloud.rotation.x = Math.PI/2;
                 this.cloud.scale.multiplyScalar(0.01);
@@ -225,23 +167,12 @@ export default class HaimAnimation extends THREE.Object3D {
             this.add( this.spine );
         });
 
-
-        // FBO_PARTICLES
-        let particleGeo = new THREE.BoxGeometry(5,5,5);
-        let positions = this.initParticles( particleGeo );
-        this.rttIn = positions;
-        this.initFBOParticle( positions );
-        // let dotText = tex_loader.load( this.BASE_PATH + '/images/dot.png', (text) => {
-        //     dotText = text;
-        //     this.initFBOParticle();
-        // } );
-
         //
         this.loadingManager.itemEnd("HaimAnim");
     }
 
     initSPEParicle( tex_loader) {
-        let particleTex = tex_loader.load(this.BASE_PATH + '/images/tubeBag_ani_2.png');
+        let particleTex = tex_loader.load(this.BASE_PATH + '/images/tubeBag_ani_3.png');
 
         this.particleGroup = new SPE.Group({
             texture: {
@@ -282,10 +213,10 @@ export default class HaimAnimation extends THREE.Object3D {
                 //     spread: [0,-0.5,0.5]
                 // },
                 opacity: {
-                    value: [.5,1,1,1,0]
+                    value: [.5,1,1,1,1,0]
                 },
                 size: {
-                    value: [2,4,5,6,3],
+                    value: [1,2,3,3,2],
                     spread: 1
                 },
                 particleCount: 15
@@ -297,46 +228,6 @@ export default class HaimAnimation extends THREE.Object3D {
         }
         this.add( this.particleGroup.mesh );
 
-    }
-
-    initFBOParticle( positions ) {
-        this.simulationShader = new THREE.ShaderMaterial({
-            uniforms: {
-                positions: { type: "t", value: positions },
-                timer: { type: "f", value: 0 },
-                maxDepth : { type: "f", value: this.maxDepth },
-                maxDistance: { type: "f", value: 50 },
-                amplitude: { type: "f", value: 0.004 },
-                frequency: { type: "f", value: 0.4 }
-                // acce: { type: "f", value: 0 }
-            },
-            vertexShader: this.simulation_vs,
-            fragmentShader:  this.simulation_fs,
-        });
-
-        this.renderShader = new THREE.ShaderMaterial( {
-            uniforms: {
-                positions: { type: "t", value: null },
-                pointSize: { type: "f", value: 2 }
-            },
-            vertexShader: this.render_vs,
-            fragmentShader: this.render_fs,
-            transparent: true,
-            blending:THREE.AdditiveBlending,
-        } );
-
-        // Particle geometry? Just once particle
-        var particleGeometry  = new THREE.Geometry();
-        // particleGeometry.vertices.push(new THREE.Vector3(), new THREE.Vector3(0, -0.05, -0.1), new THREE.Vector3(0,-0.05,0.1));
-        particleGeometry.vertices.push( new THREE.Vector3() );
-        // particleGeometry.vertices.push(new THREE.Vector3(), new THREE.Vector3(-0.1, -0.05, 0), new THREE.Vector3(0.1, -0.05, 0), new THREE.Vector3(0,0.1,0));
-
-        this.fbo = new FBO();
-        this.fbo.init( this.width,this.height, this.renderer, this.simulationShader, this.renderShader, particleGeometry );
-        this.add( this.fbo.particles );
-        this.timerAnim = null;
-        this.fbo.particles.position.y = 1000;
-        // this.fbo.update();
     }
 
     createGroundPuddle( tex_loader) {
@@ -399,13 +290,14 @@ export default class HaimAnimation extends THREE.Object3D {
             let lengthhh = liquidGeo.vertices.length-1;
             theBag.position.copy( liquidGeo.vertices[lengthhh] );
             theBag.scale.multiplyScalar( this.clamp(this.lookupTable[j], 0.3, 1) );
-            theBag.rotation.y = -rot.y;
+            theBag.rotation.y = -rot.y + this.lookupTable[j]/2;
             tubeObject.add( theBag );
 
             // children_3 -> COULDS
             let theCloud = this.cloud.clone();
             lengthhh = liquidGeo.vertices.length-1;
             theCloud.position.copy( liquidGeo.vertices[lengthhh] );
+            // theCloud.position.y += (this.lookupTable[j]*2);
             tubeObject.add( theCloud );
 
             tubeObject.position.copy( pos );
@@ -531,6 +423,9 @@ export default class HaimAnimation extends THREE.Object3D {
         for(let i=0; i<this.tubes.length; i++){
             targets.push( this.tubes[i].children[0].morphTargetInfluences );
         }
+        // hide the liquid
+        this.tubes[0].children[1].visible = false;
+
         this.tl = new TimelineMax();    //{repeat: -1}
         this.tl.to( targets, _duration, { endArray: tmpEndArray, ease: Power0.easeNone, onStart: ()=>{
                     this.tubes[0].children[1].material.map.offset.x=-1.5;
@@ -538,6 +433,7 @@ export default class HaimAnimation extends THREE.Object3D {
                .to( targets, _duration, { endArray: tmpEndArray2, ease: Power0.easeNone })
                .to( targets, _duration, { endArray: tmpEndArray3, ease: Power0.easeNone })
                .to( targets, _duration/1.5, { endArray: tmpEndArray4, ease: Back.easeInOut.config(2.5), onStart: ()=>{
+                    this.tubes[0].children[1].visible = true;
                     this.tubes[0].children[1].material.map.offset.x=-1.5;
                     this.liquidDown = true;
                }, onComplete: ()=>{
@@ -574,77 +470,6 @@ export default class HaimAnimation extends THREE.Object3D {
                         } } );
                     }
                } });
-    }
-
-    loadModelClock(model, modelB, modelC, modelD, meshMat) {
-
-        let loader = new THREE.JSONLoader(this.loadingManager);
-        let cloMat = meshMat;
-        let myClock = new THREE.Object3D();
-        this.myCP1 = new THREE.Object3D();
-        this.myCP2 = new THREE.Object3D();
-        this.grandFatherClock = new THREE.Object3D();
-        this.pointer1Time = 0;
-        this.pointer2Time = 0;
-
-        loader.load(model, (geometry, material)=>{
-            geometry.center();
-            let cFace = new THREE.Mesh(geometry, cloMat);
-            cFace.scale.set(1, 1, 1.7);
-            cFace.position.set(0, 0, 3.1);
-            myClock.add(cFace);
-        });
-
-        loader.load(modelB, (geometryB, material)=>{
-            geometryB.center();
-            this.transY(geometryB, 27);
-            this.cGear = new THREE.Mesh(geometryB, cloMat);
-            let myGear = new THREE.Object3D();
-            myGear.add(this.cGear);
-            myGear.rotation.z = -Math.PI/5;
-            myClock.add(myGear);
-        });
-
-        loader.load(modelC, (geometryC, material)=>{
-            geometryC.center();
-            let cP1 = new THREE.Mesh(geometryC, cloMat);
-            cP1.position.set(-2.2, 0, 6);
-            cP1.scale.set(1, 1, 2.5);
-            this.myCP1.add(cP1);
-            myClock.add(this.myCP1);
-            //
-            // TweenMax.to(this.myCP1.rotation, 0.5, {z:"+="+0.1, repeat:-1, repeatDelay:2});
-        });
-
-        loader.load(modelD, (geometryD, material)=>{
-            geometryD.center();
-            let cP2 = new THREE.Mesh(geometryD, cloMat);
-            cP2.position.set(0,12,3);
-            cP2.scale.set(1, 1, 2.5);
-            this.myCP2.add(cP2);
-            myClock.add(this.myCP2);
-            //
-            // TweenMax.to(this.myCP2.rotation, 0.5, {z:"+="-0.1, repeat:-1, repeatDelay:2.6});
-
-            myClock.position.y = -80;
-            this.grandFatherClock.add(myClock);
-
-            for(let i=0; i<6; i++){
-                let geoTemp = new THREE.CylinderGeometry(0.5 ,0.5 ,90);
-                let bar = new THREE.Mesh(geoTemp, cloMat);
-                bar.position.y = -15;
-                bar.position.x = i*3 - 8;
-                this.grandFatherClock.add(bar);
-            }
-
-            this.grandFatherClock.scale.multiplyScalar(0.01);
-            this.grandFatherClock.position.set(1, 3, -1.2);
-            // this.grandFatherClock.rotation.y = Math.PI;
-
-            this.add(this.grandFatherClock);
-
-        });
-
     }
 
     transX(geo, n){
@@ -684,8 +509,7 @@ export default class HaimAnimation extends THREE.Object3D {
 
     update(dt,et) {
         // test
-        // this.particleGroup.tick( dt );
-        this.fbo.update();
+        this.particleGroup.tick( dt );
 
         if(this.liquidDown && !this.liquidOut){
             this.tubes[0].children[1].material.map.offset.x+=0.01;
@@ -704,7 +528,7 @@ export default class HaimAnimation extends THREE.Object3D {
             // this.fbo.update();
             // SPE
             // this.particleGroup.tick( dt );
-            // this.puddleAnimator.updateWithOrder( 300*dt );
+            this.puddleAnimator.updateWithOrder( 300*dt );
         }
     }
 }
