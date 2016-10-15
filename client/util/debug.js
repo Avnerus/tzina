cbbb326ba6c028f1c7a7c7143b05837ac04332c3
jhclaura <jhclaura@gmail.com>
2016-10-15 18:43:43 -0400

    meir: update bird animation and character disappearing

--- client/util/debug.js
+++ client/util/debug.js
@@ -22,28 +22,28 @@ export default {
         events.emit("add_gui", {folder:name + " - Position", listen: listen, step: 0.01}, object.position, "y", min, max * 2);
         events.emit("add_gui", {folder:name + " - Position", listen: listen, step: 0.01}, object.position, "z", min, max);
 
-        if (angles) {
-            object.angleRotation = {x: angles[0], y: angles[1], z: angles[2]};
-        } else {
-            object.angleRotation = {x: 0, y: 0, z: 0};
-        }
-        events.emit("add_gui", {
-                    folder:name + " - Rotation",
-                    onChange: () => {object.rotation.x = object.angleRotation.x * Math.PI / 180}
-        }, object.angleRotation, "x", 0, 360); 
-        events.emit("add_gui", {
-                    folder:name + " - Rotation",
-                    onChange: () => {object.rotation.y = object.angleRotation.y * Math.PI / 180}
-        }, object.angleRotation, "y", 0, 360); 
-        events.emit("add_gui", {
-                    folder:name + " - Rotation",
-                    onChange: () => {object.rotation.z = object.angleRotation.z * Math.PI / 180}
-        }, object.angleRotation, "z", 0, 360);
+        // if (angles) {
+        //     object.angleRotation = {x: angles[0], y: angles[1], z: angles[2]};
+        // } else {
+        //     object.angleRotation = {x: 0, y: 0, z: 0};
+        // }
+        // events.emit("add_gui", {
+        //             folder:name + " - Rotation",
+        //             onChange: () => {object.rotation.x = object.angleRotation.x * Math.PI / 180}
+        // }, object.angleRotation, "x", 0, 360); 
+        // events.emit("add_gui", {
+        //             folder:name + " - Rotation",
+        //             onChange: () => {object.rotation.y = object.angleRotation.y * Math.PI / 180}
+        // }, object.angleRotation, "y", 0, 360); 
+        // events.emit("add_gui", {
+        //             folder:name + " - Rotation",
+        //             onChange: () => {object.rotation.z = object.angleRotation.z * Math.PI / 180}
+        // }, object.angleRotation, "z", 0, 360);
 
-        object.global = object.scale.x;
-        events.emit("add_gui", {folder:name + " - Scale", listen: listen, step: 0.01, onChange: () => {
-           object.scale.set(object.global, object.global, object.global);
-        }}, object, "global",0,4); 
+        // object.global = object.scale.x;
+        // events.emit("add_gui", {folder:name + " - Scale", listen: listen, step: 0.01, onChange: () => {
+        //    object.scale.set(object.global, object.global, object.global);
+        // }}, object, "global",0,4); 
     },
 
     fastForward : function(video) {
