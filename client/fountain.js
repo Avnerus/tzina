86e85ff42efc3143df937e782acde05fc7a7af64
Laura Chen <jhclaura@gmail.com>
2016-10-19 08:31:11 -0400

    add weird fountain particle animation

--- client/fountain.js
+++ client/fountain.js
@@ -8,7 +8,7 @@ export default class Fountain extends THREE.Object3D  {
 
         this.downVelocity = new THREE.Vector3(1.5,6,0);
         this.upVelocity = new THREE.Vector3(1.5,8,0);
-        this.centerVelocity = new THREE.Vector3(0,15,0);
+        this.centerVelocity = new THREE.Vector3(0,9,0);
 
         this.outerUp = true;
 
@@ -45,25 +45,28 @@ export default class Fountain extends THREE.Object3D  {
                 value: new THREE.TextureLoader(loadingManager).load(this.BASE_PATH + 'water_splash.png')
                 //value: new THREE.TextureLoader(loadingManager).load(this.BASE_PATH + 'smokeparticle.png')
             },
-            maxParticleCount: 6000
+            maxParticleCount: 20000
         });
 
 
         // Create fountains
-        
-        // First circle
         let angle = 30;
         let radius = 2.0;
-        let position = new THREE.Vector3(0,0,0);
+        let position = new THREE.Vector3(0,0.5,0);
         let rotation = 0;
 
+        // Center Water
+        this.createTrickleCenter(position, this.centerVelocity, 0x00ffff);
+        this.add(this.particleGroup.mesh);
+
+        // First circle
+        position.y = 0;
         for (let i = 0; i <= 360; i+= angle ) {
             rotation = i * Math.PI / 180;
             position.x = Math.cos(rotation) * radius;
             position.z = Math.sin(rotation) * radius;
-            this.createTrickle(position, rotation, this.downVelocity);
+            this.createTrickle(position, rotation, this.downVelocity, 0xB7C5C9);
         }
-        this.add(this.particleGroup.mesh);
 
         // Second
         position.y = -0.5;
@@ -74,12 +77,22 @@ export default class Fountain extends THREE.Object3D  {
             position.x = Math.cos(rotation) * radius;
             position.z = Math.sin(rotation) * radius;
             let backFace = (i + 180) * Math.PI / 180;
-            this.createTrickle(position, backFace, this.upVelocity);
+            this.createTrickle(position, backFace, this.upVelocity, 0xff0000);
         }
 
-        // Center Water
-        position.set(0, 1.5, 0);
-        this.createTrickleCenter(position, this.centerVelocity);
+        // EmittersGroup
+        this.centerRingEmitters = [];
+        this.firstRingEmitters = [];
+        this.secondRingEmitters = [];
+        for(let i=0; i<this.particleGroup.emitters.length; i++){
+            if(i==0) {
+                this.centerRingEmitters.push( this.particleGroup.emitters[i] );
+            } else if (i<(this.particleGroup.emitters.length-1)/2) {
+                this.firstRingEmitters.push( this.particleGroup.emitters[i] );
+            } else {
+                this.secondRingEmitters.push( this.particleGroup.emitters[i] );
+            }
+        }
 
         // Sound
         this.soundManager.loadSound(this.event12pm_file)
@@ -131,8 +144,8 @@ export default class Fountain extends THREE.Object3D  {
         setInterval(() => {
             //console.log("Fountain cycle!", this.particleGroup.emitters.length + " Emitters");
             this.outerUp = !this.outerUp;
-            for (let i = 0; i < this.particleGroup.emitters.length; i++) {
-                if (i < this.particleGroup.emitters.length / 2) {
+            for (let i = 1; i < this.particleGroup.emitters.length; i++) {
+                if (i < (this.particleGroup.emitters.length-1) / 2) {
                     this.particleGroup.emitters[i].velocity.value = this.outerUp ? this.downVelocity : this.upVelocity;
                 } else {
                     this.particleGroup.emitters[i].velocity.value = this.outerUp ? this.upVelocity : this.downVelocity;
@@ -157,6 +170,30 @@ export default class Fountain extends THREE.Object3D  {
 
     firstAni() {
         console.log("do first ani!");
+
+        for(let i=0; i<this.centerRingEmitters.length; i++){
+            let e = this.centerRingEmitters[i];
+            let target = [];
+            target[0] = e.velocity._value;
+            target[1] = e.velocity._spread;
+            target[2] = e.acceleration._value;
+            target[3] = e.acceleration._spread;
+
+            TweenMax.to( target[0], 2, { y: 19, onStart:()=>{
+                TweenMax.to( target[1], 5, {x:9, z:9});
+                TweenMax.to( target[2], 5, {y:-24});
+            },
+            onUpdateParams:[e],
+            onUpdate: (e)=>{
+                e.updateFlags.velocity = true;
+                e.updateFlags.acceleration = true;
+            },
+            onCompleteParams:[e],
+            onComplete: (e)=>{
+                e.updateFlags.velocity = true;
+                e.updateFlags.acceleration = true;
+            }} );
+        }
     }
 
     secAni() {
@@ -167,7 +204,7 @@ export default class Fountain extends THREE.Object3D  {
         console.log("do third ani!");
     }
 
-    createTrickle(position, rotation, velocity) {
+    createTrickle(position, rotation, velocity, colorCode) {
         // Get the velocity after rotation
         let emitter = new SPE.Emitter({
             maxAge: 5,
@@ -187,7 +224,7 @@ export default class Fountain extends THREE.Object3D  {
                 value: velocity
             },
             color: {
-                value: new THREE.Color(0xB7C5C9)
+                value: new THREE.Color(colorCode)
             },
             size: {
                 value: [0.2, 0.4, 0.0]
@@ -202,43 +239,38 @@ export default class Fountain extends THREE.Object3D  {
                 spread: 2
             }
         });
-
         this.particleGroup.addEmitter(emitter);
     }
 
-    createTrickleCenter(position, velocity) {
+    createTrickleCenter(position, velocity, colorCode) {
         // Get the velocity after rotation
         let emitter = new SPE.Emitter({
-            maxAge: 4,
-            type: SPE.distributions.Sphere,
+            maxAge: 5,
+            type: SPE.distributions.BOX,
             position : {
                 value: position,
-                spread: new THREE.Vector3( 0, 0, 0 )
+                spread: new THREE.Vector3( .3, 0, .3 )
             },
             acceleration: {
-                value: new THREE.Vector3(0,-12,0)
-                // spread: new THREE.Vector3( 10, 0, 10 )
+                value: new THREE.Vector3(0,-14,0),
+                spread: new THREE.Vector3( 1, 0, 1 )
             },
             velocity: {
-                value: velocity
+                value: velocity,
+                spread: new THREE.Vector3( 0, 0.5, 0 )
             },
             color: {
                 value: new THREE.Color(0xB7C5C9)
             },
             size: {
-                value: [0.2, 0.4, 0.0]
+                value: [0.3, 0.4, 0.2, 0.0]
             },
-            particleCount: 200,
+            particleCount: 1000,
             opacity: {
-                value: [0.3, 0.8, 0.5]
+                value: [0.5, 0.8, 0.4, 0.2]
             },
-            transparent: true,
-            wiggle: {
-                value: 3,
-                spread: 2
-            }
+            transparent: true
         });
-
         this.particleGroup.addEmitter(emitter);
     }
 
