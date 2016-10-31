55b36089c0069c4950d2f829bc995f3c8cadcd45
Ziv Schneider <ziv.schneider@gmail.com>
2016-10-31 13:15:53 -0400

    suns scale and position fix for 7pm

--- client/config.js
+++ client/config.js
@@ -4,7 +4,7 @@ export default {
     fps: 60,
     enableFlying: false,
     skipIntro: true,
-    startTime: 7,
+    startTime: 12,
     skipCharacters: true,
     movementSpeed: 0.08,
     //movementSpeed: 1,
