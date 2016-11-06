8b6b4fa03d1cc27ba3eccde10e693f1569cb97c0
Avnerus <avnerus@gmail.com>
2016-11-06 14:51:24 +0200

    Square rotation bug fix

--- client/extras.js
+++ client/extras.js
@@ -12,7 +12,7 @@ export default class Extras extends THREE.Object3D {
         this.currentExtras = [];
         this.camera = camera;
         this.renderer = renderer;
-        this.debug = true;
+        this.debug = false;
     }
 
     init(loadingManager) {
