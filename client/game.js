d29eff6c9aa50f0204dca9c0b779181b68354d47
Avnerus <avnerus@gmail.com>
2017-03-05 20:14:53 +0200

    Separate water drops distance for vive and desktop

--- client/game.js
+++ client/game.js
@@ -225,7 +225,7 @@ export default class Game {
         this.ZOOM_OUT_SOUND = 'assets/sound/zoom_out.ogg'
         this.SUN_GAZE_SOUND = 'assets/sound/ui/Hour_Replace_1.ogg'
 
-        this.waterDrops = new WaterDrops();
+        this.waterDrops = new WaterDrops(this.config);
         this.camera.add(this.waterDrops);
 
             /*
