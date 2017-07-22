84a05ed6376fb2ab6456e786d92f4c119e2c4e2a
Or Fleisher <contact@orfleisher.com>
2017-07-22 11:38:41 -0400

    Adding WebVR 1.1 support
    Adding vrDisplay.getFrameData as a case to the controls for newer
    browsers with Vive

--- client/tzina_vr_controls.js
+++ client/tzina_vr_controls.js
@@ -3,66 +3,66 @@
  * @author mrdoob / http://mrdoob.com
  * @author avnerus / http://avner.js.org
  */
-export default function ( emitter, object, onError, square ) {
+export default function (emitter, object, onError, square) {
 
-	var scope = this;
+    var scope = this;
 
     this.square = square;
 
-	var vrInput;
+    var vrInput;
 
-	var standingMatrix = new THREE.Matrix4();
+    var standingMatrix = new THREE.Matrix4();
 
-	function gotVRDevices( devices ) {
+    function gotVRDevices(devices) {
 
-		for ( var i = 0; i < devices.length; i ++ ) {
+        for (var i = 0; i < devices.length; i++) {
 
-			if ( ( 'VRDisplay' in window && devices[ i ] instanceof VRDisplay ) ||
-				 ( 'PositionSensorVRDevice' in window && devices[ i ] instanceof PositionSensorVRDevice ) ) {
+            if (('VRDisplay' in window && devices[i] instanceof VRDisplay) ||
+                ('PositionSensorVRDevice' in window && devices[i] instanceof PositionSensorVRDevice)) {
 
-				vrInput = devices[ i ];
-				break;  // We keep the first we encounter
+                vrInput = devices[i];
+                break; // We keep the first we encounter
 
-			}
+            }
 
-		}
+        }
 
-		if ( !vrInput ) {
+        if (!vrInput) {
 
-			if ( onError ) onError( 'VR input not available.' );
+            if (onError) onError('VR input not available.');
 
-		}
+        }
 
-	}
+    }
 
-	if ( navigator.getVRDisplays ) {
+    if (navigator.getVRDisplays) {
 
-		navigator.getVRDisplays().then( gotVRDevices );
+        navigator.getVRDisplays().then(gotVRDevices);
 
-	} else if ( navigator.getVRDevices ) {
+    } else if (navigator.getVRDevices) {
 
-		// Deprecated API.
-		navigator.getVRDevices().then( gotVRDevices );
+        // Deprecated API.
+        navigator.getVRDevices().then(gotVRDevices);
 
-	}
+    }
 
-	// the Rift SDK returns the position in meters
-	// this scale factor allows the user to define how meters
-	// are converted to scene units.
+    // the Rift SDK returns the position in meters
+    // this scale factor allows the user to define how meters
+    // are converted to scene units.
 
-	this.scale = 1;
+    this.scale = 1;
 
-	// If true will use "standing space" coordinate system where y=0 is the
-	// floor and x=0, z=0 is the center of the room.
-	this.standing = true;
+    // If true will use "standing space" coordinate system where y=0 is the
+    // floor and x=0, z=0 is the center of the room.
+    this.standing = true;
 
-	// Distance from the users eyes to the floor in meters. Used when
-	// standing=true but the VRDisplay doesn't provide stageParameters.
-	this.userHeight = 22.1;
+    // Distance from the users eyes to the floor in meters. Used when
+    // standing=true but the VRDisplay doesn't provide stageParameters.
+    this.userHeight = 22.1;
 
     this.active = true;
 
-    this.offset = new THREE.Vector3(0,0,0);
+    this.offset = new THREE.Vector3(0, 0, 0);
 
     this.BASE_VIVE = new THREE.Vector3(
         0.030513843521475792,
@@ -78,157 +78,176 @@ export default function ( emitter, object, onError, square ) {
             this.active = true;
         } else {
             this.active = false;
-        }        
+        }
     })
 
-    this.calibrate = function() {
+    this.calibrate = function () {
         console.log("CALIBRATE with square");
-            /*
-        let squareCube = new THREE.Object3D();
-        squareCube.position.set(0.49,24,11.7);
-        this.square.clockwork.add(squareCube);
+        /*
+    let squareCube = new THREE.Object3D();
+    squareCube.position.set(0.49,24,11.7);
+    this.square.clockwork.add(squareCube);
 
-        this.square.clockwork.updateMatrixWorld(true);
-        squareCube.updateMatrixWorld(true);
-        let worldPos = new THREE.Vector3().setFromMatrixPosition(squareCube.matrixWorld);
-        //worldPos.multiplyScalar(1 / 0.013);
+    this.square.clockwork.updateMatrixWorld(true);
+    squareCube.updateMatrixWorld(true);
+    let worldPos = new THREE.Vector3().setFromMatrixPosition(squareCube.matrixWorld);
+    //worldPos.multiplyScalar(1 / 0.013);
 
-        console.log("CALIBRATE - Desired world positon by square: ", worldPos);
-        worldPos.y = 12.67;
-        this.SQUARE_POSITION.copy(worldPos); */
+    console.log("CALIBRATE - Desired world positon by square: ", worldPos);
+    worldPos.y = 12.67;
+    this.SQUARE_POSITION.copy(worldPos); */
 
-            /*
+        /*
 
-        this.update();
-        let currentPosition = new THREE.Vector3().copy(object.position);
-        if (currentPosition) {
-            console.log("CALIBRATE - Current position", currentPosition.x, currentPosition.z);
-            this.offset.copy(this.BASE_VIVE).sub(currentPosition);
-            this.offset.y = 0;
-            console.log("CALIBRATE - Offset:", this.offset);
-        }*/
+    this.update();
+    let currentPosition = new THREE.Vector3().copy(object.position);
+    if (currentPosition) {
+        console.log("CALIBRATE - Current position", currentPosition.x, currentPosition.z);
+        this.offset.copy(this.BASE_VIVE).sub(currentPosition);
+        this.offset.y = 0;
+        console.log("CALIBRATE - Offset:", this.offset);
+    }*/
     }
 
     this.getCurrentPosition = function () {
-        if (vrInput) { 
-            let pose = vrInput.getPose().position;
-            if (pose) {
-                let position = new THREE.Vector3().fromArray(pose);
-                if (this.standing) {
-                    standingMatrix.fromArray(vrInput.stageParameters.sittingToStandingTransform);
-                    position.applyMatrix4(standingMatrix);
-                    return position;
+        if (vrInput) {
+            if (vrInput.getPose) {
+                let pose = vrInput.getPose().position;
+                if (pose) {
+                    let position = new THREE.Vector3().fromArray(pose);
+                    if (this.standing) {
+                        standingMatrix.fromArray(vrInput.stageParameters.sittingToStandingTransform);
+                        position.applyMatrix4(standingMatrix);
+                        return position;
+                    }
                 }
-            } else {
-                return null;
+            } else if (vrInput.getFrameData) {
+                vrInput.getFrameData(framedata);
+                pose = framedata.pose;
+                if (pose) {
+                    let position = new THREE.Vector3().fromArray(pose);
+                    if (this.standing) {
+                        standingMatrix.fromArray(vrInput.stageParameters.sittingToStandingTransform);
+                        position.applyMatrix4(standingMatrix);
+                        return position;
+                    }
+                }
             }
         } else {
             return null;
         }
     }
 
-	this.update = function () {
+    this.update = function () {
 
-        if ( vrInput ) {
+        if (vrInput) {
+            var pose;
 
-            if ( vrInput.getPose ) {
+            if (vrInput.getPose) {
 
-                var pose = vrInput.getPose();
-
-                if ( pose.orientation !== null ) {
-
-                    object.quaternion.fromArray( pose.orientation );
-                }
+                pose = vrInput.getPose();
 
 
-                if ( this.active && pose.position !== null ) {
-                    //console.log("Calibrate VR Position", pose.position);
+            } else if (vrInput.getFrameData) {
 
-                    object.position.fromArray(pose.position).multiplyScalar(this.scale);
-                    
-                    if ( this.standing ) {
+                vrDisplay.getFrameData(frameData);
 
-                        if ( vrInput.stageParameters ) {
-                            
-                            object.updateMatrix();
-
-                            standingMatrix.fromArray(vrInput.stageParameters.sittingToStandingTransform);
-
-                            object.applyMatrix( standingMatrix );
-                        } else {
-
-                            object.position.setY( object.position.y + this.userHeight );
-
-                        }
-
-                    }
-                }
+                pose = frameData.pose;
 
             } else {
 
                 // Deprecated API.
                 var state = vrInput.getState();
 
-                if ( state.orientation !== null ) {
+                if (state.orientation !== null) {
 
-                    object.quaternion.copy( state.orientation );
+                    object.quaternion.copy(state.orientation);
 
                 }
 
-                if ( state.position !== null ) {
+                if (state.position !== null) {
 
-                    object.position.copy( state.position );
+                    object.position.copy(state.position);
 
-                } 
+                }
+            }
+
+            if (pose.orientation !== null) {
+
+                object.quaternion.fromArray(pose.orientation);
+            }
+
+
+            if (this.active && pose.position !== null) {
+                //console.log("Calibrate VR Position", pose.position);
+
+                object.position.fromArray(pose.position).multiplyScalar(this.scale);
+
+                if (this.standing) {
+
+                    if (vrInput.stageParameters) {
+
+                        object.updateMatrix();
+
+                        standingMatrix.fromArray(vrInput.stageParameters.sittingToStandingTransform);
+
+                        object.applyMatrix(standingMatrix);
+                    } else {
+
+                        object.position.setY(object.position.y + this.userHeight);
+
+                    }
+
+                }
             }
             //console.log("Calibrate VR Position", pose.position);
 
         }
-	};
+    };
 
-	this.resetPose = function () {
+    this.resetPose = function () {
 
-		if ( vrInput ) {
+        if (vrInput) {
 
-			if ( vrInput.resetPose !== undefined ) {
+            if (vrInput.resetPose !== undefined) {
                 console.log("CALIBRATE resetPose");
 
-				vrInput.resetPose();
+                vrInput.resetPose();
 
-			} else if ( vrInput.resetSensor !== undefined ) {
+            } else if (vrInput.resetSensor !== undefined) {
 
-				// Deprecated API.
-				vrInput.resetSensor();
+                // Deprecated API.
+                vrInput.resetSensor();
 
-			} else if ( vrInput.zeroSensor !== undefined ) {
+            } else if (vrInput.zeroSensor !== undefined) {
 
-				// Really deprecated API.
-				vrInput.zeroSensor();
+                // Really deprecated API.
+                vrInput.zeroSensor();
 
-			}
+            }
 
-		}
+        }
 
-	};
+    };
 
-	this.resetSensor = function () {
+    this.resetSensor = function () {
 
-		console.warn( 'THREE.VRControls: .resetSensor() is now .resetPose().' );
-		this.resetPose();
+        console.warn('THREE.VRControls: .resetSensor() is now .resetPose().');
+        this.resetPose();
 
-	};
+    };
 
-	this.zeroSensor = function () {
+    this.zeroSensor = function () {
 
-		console.warn( 'THREE.VRControls: .zeroSensor() is now .resetPose().' );
-		this.resetPose();
+        console.warn('THREE.VRControls: .zeroSensor() is now .resetPose().');
+        this.resetPose();
 
-	};
+    };
 
-	this.dispose = function () {
+    this.dispose = function () {
 
-		vrInput = null;
+        vrInput = null;
 
-	};
+    };
 
 };
